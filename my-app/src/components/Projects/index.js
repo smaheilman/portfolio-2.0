11309f5fd27cd85f35e3093d88bb0b3d2382db75
Shmee <smaheilman@gmail.com>
2022-01-15 15:44:42 -0500

    fixed images

--- my-app/src/components/Projects/index.js
+++ my-app/src/components/Projects/index.js
@@ -4,70 +4,54 @@ function Projects() {
     const [projects] = useState([
         {
             name: 'Budget Buddy',
+            path: 'budget-buddy-screenshot.jpg',
+            link: 'https://infinite-sea-98511.herokuapp.com/',
             description: 'A app to help with monthly budgeting!'
         },
         {
             name: 'Coding Quiz',
+            path: 'quiz-screenshot.jpg',
+            link: 'https://smaheilman.github.io/challenge-4-os/',
             description: 'A website to test you coding skills, quizing you on basic coding questions.'
         },
         {
             name: 'Movie Genator',
+            path: 'Movie-generator-screenshot.jpg',
+            link: 'https://benrowen5.github.io/What-Should-I-Watch/',
             description: 'An app to help you decide what movie to watch tonight based on rating, genre and viewer score'
         },
         {
             name: 'Daily Schedule',
+            path: 'schedule-app-screenshot.jpg',
+            link: 'https://smaheilman.github.io/challenge-5/',
             description: 'Used to create a daily schedule.'
         },
         {
             name: 'Weather App',
+            path: 'weather-app-screenshot.jpg',
+            link: 'https://smaheilman.github.io/weather-dashboard-challenge-6/',
             description: 'Recieves the weather for the week for whatever location you search for.'
         },
         {
             name: 'Budget Tracker',
+            path: 'budget-tracker-screenshot.jpg',
+            link: 'https://peaceful-savannah-76507.herokuapp.com/',
             description: 'Keeps track of you incoming and out going expenses.'
         }
     ])
 
     return (
         <div>
-            <ul>
-                <li>
-                    <a href="https://infinite-sea-98511.herokuapp.com/">
-                    <img src='../../assets/screenshots/budget-buddy-screenshot.png'
-                        alt='Budget Buddy' />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://benrowen5.github.io/What-Should-I-Watch/">
-                        <img src='../../assets/screenshots/Movie-generator-screenshot.png'
-                            alt='Movie Generator' />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://smaheilman.github.io/challenge-4-os/">
-                        <img src='../../assets/screenshots/quiz-screenshot.png'
-                            alt='Quiz' />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://smaheilman.github.io/weather-dashboard-challenge-6/">
-                        <img src='../../assets/screenshots/weather-app-screenshot.png'
-                            alt='Weather app' />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://smaheilman.github.io/challenge-5/">
-                        <img src='../../assets/screenshots/schedule-app-screenshot.png'
-                            alt='Daily Schedule App'/>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://peaceful-savannah-76507.herokuapp.com/">
-                        <img src='../../assets/screenshots/budget-tracker-screenshot.png'
-                            alt='Budget Tracker'/>
-                    </a>
-                </li>
-            </ul>
+            {projects.map((image)=> (
+              <a href={image.link}>
+                <img src={require(`../../assets/screenshots/${image.path}`)}
+                  alt={image.name}
+                  key={image.name}
+                  width = '10%'
+                  height= '10%'
+                />
+              </a>
+            ))}
         </div>
     )
 
