2b1bd0c757337a88dc36df8796049553661c5d71
Shmee <smaheilman@gmail.com>
2022-01-18 19:48:43 -0500

    swap icons

--- my-app/src/components/Footer/index.js
+++ my-app/src/components/Footer/index.js
@@ -11,10 +11,10 @@ function Footer() {
             <a className="media" href="https://twitter.com/smaheilman">
                     <img src={require(`../../assets/icons/connection.png`)} alt="twitter-icon"></img>
             </a>
-            <a className="media" href="https://www.facebook.com/sam.heilman.5">
+            <a className="media" href="https://www.linkedin.com/in/samantha-heilman-709282172/">
                     <img src={require(`../../assets/icons/linkdin.png`)} alt="link-icon"></img>
             </a>
-            <a className="media" href="https://www.linkedin.com/in/samantha-heilman-709282172/">
+            <a className="media" href="https://www.facebook.com/sam.heilman.5">
                     <img src={require(`../../assets/icons/neon.png`)} alt="face-icon"></img>
             </a>
             
