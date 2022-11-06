c724d102c7b65f50aacddeef6548d68764cdd25d
Shmee <smaheilman@gmail.com>
2022-11-06 14:54:28 -0500

    fix res

--- my-app/src/components/Projects/index.js
+++ my-app/src/components/Projects/index.js
@@ -57,7 +57,7 @@ function Projects() {
             <h2 className="proh">
                 Projects
                 <span className="resume">
-                    <a className='resLink' href={require(`../../assets/SamHeilmanResume (1).pdf`)}>
+                    <a className='resLink' href={require(`../../assets/SamHeilmanResx.pdf`)}>
                         <img src={require(`../../assets/icons/resume.png`)} alt='resume icon'></img>
                         Resume
                     </a>
