82c5a796151cb6e9977f5abf08aa87802852bb2e
Shmee <smaheilman@gmail.com>
2024-04-26 08:57:05 -0400

    Change about section

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -8,9 +8,11 @@ function About() {
         <p className="About-Text">
           Hi! My name is Samantha Heilman and I am a web developer. I grew up on
           a farm in northeastern Ohio near Cleveland and still live in the same
-          area. I originally went to school and studied biology and chemistry
-          and worked in the food production industry, but have recently decided
-          to change career paths for web development!
+          area. I spend my free time playing games with friends, walking my dog,
+          and working on projects around the house. I originally went to school
+          and studied biology and chemistry and worked in the food production
+          industry. I am passionate about developing sites that are user
+          friendly and accesible.
         </p>
       </div>
       <img
